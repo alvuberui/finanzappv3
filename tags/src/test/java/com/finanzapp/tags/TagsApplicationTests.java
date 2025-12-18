e606bc3d9dff02025261e6c6206893dc5f037d07
Álvaro Úbeda Ruiz <alvaroubeda8@gmail.com>
2025-12-18 20:00:08 +0100

    initial configuration

---
+++ tags/src/test/java/com/finanzapp/tags/TagsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanzapp.tags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user/src/test/java/com/finanzapp/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanzapp.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
